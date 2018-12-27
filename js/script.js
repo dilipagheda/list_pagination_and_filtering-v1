3cb92771d16a8bb8d51fad39699dcaa4ceb9721b
Dilip <dilip_agheda@yahoo.com>
2018-12-27 11:39:06 +1100

    added keyup listener to input field so that user gets dynamic search results with pagination as phrase is being typed

--- js/script.js
+++ js/script.js
@@ -3,19 +3,8 @@ Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
    
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
+//global variables
 let studentRecords = document.getElementsByClassName('student-item');
 const PAGESIZE = 10;
 
@@ -25,8 +14,12 @@ if(studentRecords.length>0){
    searchDiv.classList.add('student-search');
    let inputBox = document.createElement('input');
    inputBox.placeholder='Search for students...';
+
+   //add keyup listener to inputbox so that search happens in real time as user types
+   inputBox.addEventListener('keyup', search);
    let button = document.createElement('button');
    button.innerText = 'Search';
+
    //add listener to the button
    button.addEventListener("click",search);
    searchDiv.appendChild(inputBox);
@@ -62,22 +55,9 @@ function search(){
    appendPageLinks();
 }
 
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
 
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
 /*** 
- This function takes page number as an argument that could be any value greater than 1.
+ This function- showPage- takes page number as an argument that could be any value greater than 1.
  It will then calculate from index and to index based on page size.
  It takes into consideration that index in studentRecords array starts from 0.
  so page 1 means we want to show records 0 to 9.
@@ -85,7 +65,6 @@ function search(){
 function showPage(pageNumber,searchPhrase){
    let fromIndex = PAGESIZE * (pageNumber-1);
    let toIndex = fromIndex + (PAGESIZE-1);
-   console.log(`From:${fromIndex} To:${toIndex}`);
 
    //filter based on class matched
    let matchedStudents = Array.from(studentRecords).filter(a => a.classList.contains('matched'));
@@ -99,21 +78,14 @@ function showPage(pageNumber,searchPhrase){
    }
 }
 
-
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
 /*** 
  This function creates links at the bottom of the page.
  It uses helper function generateLIs to generate li tags based on total pages
  * ***/
 function appendPageLinks(){
-
-   
    let matchedStudents = Array.from(studentRecords).filter(a => a.classList.contains('matched'));
-
    let totalPages = Math.ceil(matchedStudents.length / PAGESIZE);
+
    //first remove previous pagination
    let page = document.querySelector('.page');
    let paginationDiv = document.querySelector('.pagination');
@@ -142,6 +114,7 @@ function appendPageLinks(){
    }
 }
 
+//Function to generate li elements
 function generateLIs(totalPages){
    let htmlString="";
    for(let i=1;i<=totalPages;i++){
@@ -174,5 +147,6 @@ function resetClassForLIs(){
    }
 
 }
+
+//initial call on first page load
 appendPageLinks();
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
